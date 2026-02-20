e3da414d897fff216e7ace7739766ad2ae41dc32
samiyevuz <159231850+samiyevuz@users.noreply.github.com>
2026-02-20 13:21:17 +0500

    LOGO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,8 +19,8 @@
 
                 {{-- Logo --}}
                 <a href="{{ route('home') }}" class="flex items-center shrink-0">
-                    @if(file_exists(public_path('images/logo.jpg')))
-                        <img src="{{ asset('images/logo.jpg') }}" alt="CTS" class="h-12 w-auto" onerror="this.style.display='none'; this.nextElementSibling.style.display='block';">
+                    @if(file_exists(public_path('images/logo.svg')))
+                        <img src="{{ asset('images/logo.svg') }}" alt="CTS" class="h-12 w-auto" onerror="this.style.display='none'; this.nextElementSibling.style.display='block';">
                         <span class="text-4xl font-black tracking-tight text-gray-900 leading-none" style="display:none;">
                             <span style="font-style:italic;letter-spacing:-3px;opacity:0.45">C</span><span style="letter-spacing:-1px">TS</span>
                         </span>
